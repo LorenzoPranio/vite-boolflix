00014cacdef7c573493bc52d66d615da9390ba32
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-27 16:41:32 +0200

    [UPD]Added star vote and finished MILESTONE 3

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -13,9 +13,20 @@ export default{
       };
       return languageCountry[languageCode] || languageCode;
     };
+    const getStar = (voteAverage, starIndex) => {
+        const ratingOutOfFive = (voteAverage / 10) * 5;
+        const fullStars = Math.floor(ratingOutOfFive);
+
+        if (starIndex <= fullStars) {
+            return 'bi bi-star-fill text-warning';
+        } else {
+            return 'bi bi-star';
+        }
+    };
     return {
       store,
-      getFlag
+      getFlag,
+      getStar
     }
   },
 }
@@ -28,9 +39,13 @@ export default{
             <ul>
                 <li v-for="item in store.resultsMovies" :key="item.id">
                   <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.title}`">
-                  {{ item.title }} ({{ item.original_title }}) ({{ item.vote_average }})
+                  {{ item.title }} ({{ item.original_title }})
                   <span :class="`fi fi-${getFlag(item.original_language)}`"></span>
-                  {{ item.poster_path }}
+                  <span class="rating">
+                  <span v-for="n in 5" :key="n">
+                    <i :class="getStar(item.vote_average, n)"></i>
+                  </span>
+                  </span>
                 </li>
             </ul>
         </div>
@@ -39,8 +54,13 @@ export default{
             <ul>
                 <li v-for="item in store.resultsSeriesTV" :key="item.id">
                   <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.title}`">
-                  {{ item.name }} ({{ item.original_title }}) ({{ item.vote_average }})
+                  {{ item.name }} ({{ item.original_title }})
                   <span :class="`fi fi-${getFlag(item.original_language)}`"></span>
+                  <span class="rating">
+                  <span v-for="n in 5" :key="n">
+                    <i :class="getStar(item.vote_average, n)"></i>
+                  </span>
+                  </span>
                 </li>
             </ul>
         </div>
